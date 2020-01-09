8bd5db700a2368657083030e56a5b0335262dfa9
Mathilde G <mathilde.guillossou@gmail.com>
2020-01-09 22:28:26 +0100

    add scan for NON BLE devices ... Since wordclock is not a BLE device ...

--- app/src/main/java/com/gui/wordclock/ScanActivity.kt
+++ app/src/main/java/com/gui/wordclock/ScanActivity.kt
@@ -1,15 +1,23 @@
 package com.gui.wordclock
 
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.IntentFilter
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_scan.*
+
 
 class ScanActivity : AppCompatActivity() {
 
     companion object {
+        private const val TAG = "WordClock::ScanActivity"
         private const val REQUEST_ENABLE_BT = 200
     }
 
@@ -25,9 +33,54 @@ class ScanActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan)
         check()
+
+        /*val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter?.bondedDevices
+        pairedDevices?.forEach { device ->
+            val deviceName = device.name
+            val deviceHardwareAddress = device.address // MAC address
+        }*/
+
+        scan_btn.setOnClickListener {
+            bluetoothAdapter?.startDiscovery()
+            indeterminateBar.visibility = View.VISIBLE
+            scan_btn.isEnabled = false
+        }
+
+
+        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        registerReceiver(receiver, filter)
+        //98:D3:61:F9:2B:FD
     }
 
-    fun check() {
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            when(intent.action) {
+                BluetoothDevice.ACTION_FOUND -> {
+                    scan_btn.isEnabled = true
+                    // Discovery has found a device. Get the BluetoothDevice
+                    // object and its info from the Intent.
+                    val device: BluetoothDevice? =
+                        intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+                    val deviceName = device?.name
+                    val deviceHardwareAddress = device?.address // MAC address
+                    Log.d("device_add", "$deviceHardwareAddress")
+                    Log.d("device", "$deviceName")
+                    if(device?.address.equals("98:D3:61:F9:2B:FD")) {
+                        indeterminateBar.visibility = View.GONE
+                        wordclock.visibility = View.VISIBLE
+                        wordclock.text = "wordclock founded at the following address: $device"
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
+    }
+
+    private fun check() {
         bluetoothAdapter?.takeIf { it.isDisabled }?.apply {
             val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
