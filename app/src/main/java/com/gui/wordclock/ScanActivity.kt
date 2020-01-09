e4ba920d382a136746770632a08be74c56142382
Mathilde G <mathilde.guillossou@gmail.com>
2020-01-09 21:27:30 +0100

    Automaticaly turn bluetooth ON

--- app/src/main/java/com/gui/wordclock/ScanActivity.kt
+++ app/src/main/java/com/gui/wordclock/ScanActivity.kt
@@ -1,12 +1,36 @@
 package com.gui.wordclock
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class ScanActivity : AppCompatActivity() {
 
+    companion object {
+        private const val REQUEST_ENABLE_BT = 200
+    }
+
+    private val BluetoothAdapter.isDisabled: Boolean
+        get() = !isEnabled
+
+    private val bluetoothAdapter: BluetoothAdapter? by lazy(LazyThreadSafetyMode.NONE) {
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothManager.adapter
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan)
+        check()
+    }
+
+    fun check() {
+        bluetoothAdapter?.takeIf { it.isDisabled }?.apply {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+        }
     }
 }
